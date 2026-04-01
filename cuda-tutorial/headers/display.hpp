80e4282364c145498dd5fa26603df9e6cca18eca
Hristiyan Shterev <hristiyan.haralambiev.shterev@gmail.com>
2026-04-01 16:46:51 +0300

    added missing cell and fixed my display header

--- cuda-tutorial/headers/display.hpp
+++ cuda-tutorial/headers/display.hpp
@@ -2,16 +2,11 @@
 #include <fstream>
 #include <sstream>
 
-#include "xeus/xbase64.hpp"
-#include "xeus/xinterpreter.hpp"
 #include "nlohmann/json.hpp"
-#include "xcpp/xdisplay.hpp"
 
-void show_image(std::string path, int width) {
-    nlohmann::json bundle;
-    bundle["text/html"] = "<img src='" + path + "' style='width: " + std::to_string(width) + "%;'>";
-    xeus::get_interpreter().display_data(bundle, nlohmann::json::object(), nlohmann::json::object());
-}
+#include "xeus/xbase64.hpp"
+
+#include "xcpp/xdisplay.hpp"
 
 namespace nl = nlohmann;
 
